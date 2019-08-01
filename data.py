8da85130a5d2f1756711639797e20c931157e05e
Byambasuren <ibyambasuren@gmail.com>
2019-08-01 16:19:12 +0800

    Add files via upload
    CLASSIFYING WORD GROUPS WITH A CHARACTER-LEVEL RNN

---
+++ data.py
@@ -0,0 +1,53 @@
+import torch
+import glob
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters)
+
+def findFiles(path): return glob.glob(path)
+#print(findFiles('../data/verb/*.txt'))
+
+# Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+#print('Ślusàrski')
+	
+# Read a file and split into lines
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [line for line in lines]
+#    return [unicodeToAscii(line) for line in lines]
+
+# Build the category_lines dictionary, a list of lines per category
+category_lines = {}
+all_categories = []
+#for filename in findFiles('data/names/*.txt'):
+for filename in findFiles('../data/verb/*.txt'):   
+    category = filename.split('/')[-1].split('.')[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+#print('n_categories =', n_categories)
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+	
+#print(letterToIndex('а'))
+#print(lineToTensor('баатар').size())
---
+++ train.py
@@ -0,0 +1,145 @@
+import torch
+from data import *
+from model import *
+import random
+import time
+import math
+
+#n_hidden = 128
+n_hidden = 32
+n_epochs = 100000
+print_every = 5000
+plot_every = 1000
+learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+
+def categoryFromOutput(output):
+    top_n, top_i = output.data.topk(1) # Tensor out of Variable with .data
+    category_i = top_i[0][0]
+    return all_categories[category_i], category_i
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingPair():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
+    line_tensor = Variable(lineToTensor(line))
+    return category, line, category_tensor, line_tensor
+
+rnn = RNN(n_letters, n_hidden, n_categories)
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+criterion = nn.NLLLoss()
+
+#for i in range(10):
+#    category, line, category_tensor, line_tensor = randomTrainingPair()
+#    print('category =', category, '/ line =', line)
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+    optimizer.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    optimizer.step()
+
+    return output, loss.data
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for epoch in range(1, n_epochs + 1):
+	# Get a random training input and target
+    category, line, category_tensor, line_tensor = randomTrainingPair()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print epoch number, loss, name and guess
+    if epoch % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch / n_epochs * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    if epoch % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+torch.save(rnn, 'char-rnn-classification_egshigt_giiguulegch.pt')
+#torch.save(rnn, 'char-rnn-classification.pt')
+
+
+
+
+
+# Plotting the historical loss from all_losses shows the network learning.
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+plt.figure()
+plt.plot(all_losses)
+
+
+
+
+
+# To see how well the network performs on different categories, 
+# we will create a confusion matrix, indicating for every actual language (rows) 
+# which language the network guesses (columns).
+
+# Keep track of correct guesses in a confusion matrix
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+# Just return an output given a line
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+# Go through a bunch of examples and record which are correctly guessed
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output = evaluate(line_tensor)
+    guess, guess_i = categoryFromOutput(output)
+    category_i = all_categories.index(category)
+    confusion[category_i][guess_i] += 1
+
+# Normalize by dividing every row by its sum
+for i in range(n_categories):
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+# Set up plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+# Set up axes
+ax.set_xticklabels([''] + all_categories, rotation=90)
+ax.set_yticklabels([''] + all_categories)
+
+# Force label at every tick
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+# sphinx_gallery_thumbnail_number = 2
+plt.show()
